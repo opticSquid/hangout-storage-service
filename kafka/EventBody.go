73981cdc0dbabc3e2d675062d4192f18063d6efb
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-07-15 15:07:35 +0530

    removed kafka event key

--- kafka/EventBody.go
+++ kafka/EventBody.go
@@ -1,6 +1,7 @@
 package kafka
 
 type eventBody struct {
-	Filename string `json:"filename"`
-	UserId   int32  `json:"userId"`
+	Filename    string `json:"filename"`
+	ContentType string `json:"contentType"`
+	UserId      int32  `json:"userId"`
 }
