bdc880d5de69bdf899841f95bf80bdbd5c18760a
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-07-19 15:22:58 +0530

    added open telemetry  config file

--- telemetry/otel-init.go
+++ telemetry/otel-init.go
@@ -12,18 +12,22 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/log"
 	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 	"golang.org/x/net/context"
+	"hangout.com/core/storage-service/logger"
 )
 
 // SetupOTelSDK bootstraps the OpenTelemetry pipeline. If it does not return an error, make sure to call shutdown for proper cleanup.
-func SetUpOTelSDK(ctx context.Context, cfg *koanf.Koanf) (shutdown func(context.Context) error, err error) {
+func SetUpOTelSDK(ctx context.Context, cfg *koanf.Koanf, log logger.Log) (shutdown func(context.Context) error, err error) {
 	var shutdownFuncs []func(context.Context) error
 
 	// shutdown calls cleanup functions registered via shutdownFuncs.
 	// The errors from the calls are joined.
 	// Each registered cleanup will be invoked once.
 	shutdown = func(ctx context.Context) error {
+		log.Info(ctx, "shutting down telemetry")
 		var err error
 		for _, fn := range shutdownFuncs {
 			err = errors.Join(err, fn(ctx))
@@ -37,12 +41,15 @@ func SetUpOTelSDK(ctx context.Context, cfg *koanf.Koanf) (shutdown func(context.
 		err = errors.Join(inErr, shutdown(ctx))
 	}
 
+	// Set up resource
+	res := resource.NewWithAttributes(semconv.SchemaURL, semconv.ServiceName(cfg.String("application.name")))
+
 	// Set up propagator.
 	prop := newPropagator()
 	otel.SetTextMapPropagator(prop)
 
 	// Set up trace provider.
-	tracerProvider, err := newTracerProvider(ctx, cfg)
+	tracerProvider, err := newTracerProvider(ctx, cfg, res)
 	if err != nil {
 		handleErr(err)
 		return
@@ -51,7 +58,7 @@ func SetUpOTelSDK(ctx context.Context, cfg *koanf.Koanf) (shutdown func(context.
 	otel.SetTracerProvider(tracerProvider)
 
 	// Set up meter provider.
-	meterProvider, err := newMeterProvider(ctx, cfg)
+	meterProvider, err := newMeterProvider(ctx, cfg, res)
 	if err != nil {
 		handleErr(err)
 		return
@@ -60,7 +67,7 @@ func SetUpOTelSDK(ctx context.Context, cfg *koanf.Koanf) (shutdown func(context.
 	otel.SetMeterProvider(meterProvider)
 
 	// Set up logger provider.
-	loggerProvider, err := newLoggerProvider(ctx, cfg)
+	loggerProvider, err := newLoggerProvider(ctx, cfg, res)
 	if err != nil {
 		handleErr(err)
 		return
@@ -78,42 +85,48 @@ func newPropagator() propagation.TextMapPropagator {
 	)
 }
 
-func newTracerProvider(ctx context.Context, cfg *koanf.Koanf) (*trace.TracerProvider, error) {
+func newTracerProvider(ctx context.Context, cfg *koanf.Koanf, res *resource.Resource) (*trace.TracerProvider, error) {
 	traceExporter, err := otlptracegrpc.New(ctx,
 		otlptracegrpc.WithEndpoint(cfg.String("otel.endpoint")),
-		otlptracegrpc.WithCompressor("gzip"))
+		otlptracegrpc.WithCompressor("gzip"),
+		otlptracegrpc.WithInsecure())
 	if err != nil {
 		return nil, err
 	}
 	tracerProvider := trace.NewTracerProvider(
 		trace.WithBatcher(traceExporter),
+		trace.WithResource(res),
 	)
 	return tracerProvider, nil
 }
 
-func newMeterProvider(ctx context.Context, cfg *koanf.Koanf) (*metric.MeterProvider, error) {
+func newMeterProvider(ctx context.Context, cfg *koanf.Koanf, res *resource.Resource) (*metric.MeterProvider, error) {
 	metricExporter, err := otlpmetricgrpc.New(ctx,
-		otlpmetricgrpc.WithEndpoint(cfg.String("otel.endpoint")), otlpmetricgrpc.WithCompressor("gzip"))
+		otlpmetricgrpc.WithEndpoint(cfg.String("otel.endpoint")), otlpmetricgrpc.WithCompressor("gzip"),
+		otlpmetricgrpc.WithInsecure())
 	if err != nil {
 		return nil, err
 	}
 
 	meterProvider := metric.NewMeterProvider(
 		metric.WithReader(metric.NewPeriodicReader(metricExporter)),
+		metric.WithResource(res),
 	)
 	return meterProvider, nil
 }
 
-func newLoggerProvider(ctx context.Context, cfg *koanf.Koanf) (*log.LoggerProvider, error) {
+func newLoggerProvider(ctx context.Context, cfg *koanf.Koanf, res *resource.Resource) (*log.LoggerProvider, error) {
 	logExporter, err := otlploggrpc.New(ctx,
 		otlploggrpc.WithEndpoint(cfg.String("otel.endpoint")),
-		otlploggrpc.WithCompressor("gzip"))
+		otlploggrpc.WithCompressor("gzip"),
+		otlploggrpc.WithInsecure())
 	if err != nil {
 		return nil, err
 	}
 
 	loggerProvider := log.NewLoggerProvider(
 		log.WithProcessor(log.NewBatchProcessor(logExporter)),
+		log.WithResource(res),
 	)
 	return loggerProvider, nil
 }
